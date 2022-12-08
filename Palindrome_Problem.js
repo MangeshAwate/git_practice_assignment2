3ab7de50c2df1619c40fcd5e254126fca0024879
MangeshAwate <mawate9@gmail.com>
2022-12-08 15:35:22 +0530

    Update Palindrome_Problem.js
    After some changes everything works. Merge done.

--- Palindrome_Problem.js
+++ Palindrome_Problem.js
@@ -4,23 +4,19 @@ function palindrome(str){
 		bag=bag+str[i];
 	}
 	bagr="";
-	for(j=str.length;j>0;j++){
+	for(j=str.length-1;j>=0;j--){
 		bagr=bagr+str[i];
 	}
 	if(bag==bagr){
-		return false;	
+		return true;	
 	}
-	else{
-		return true;
-	}
-
 	else{
 		return false;
 	}	
 }
 
 p_check="maddam";
-result_pal=palindrama(p_check);
+result_pal=palindrome(p_check);
 console.log(result_pal);
 
 
