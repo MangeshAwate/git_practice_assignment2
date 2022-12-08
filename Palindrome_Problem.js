18050b7cc3b1674f2c93314204c1fef55b56bb4f
MangeshAwate <mawate9@gmail.com>
2022-12-08 15:18:58 +0530

    palindramaadded

--- Palindrome_Problem.js
+++ Palindrome_Problem.js
@@ -13,6 +13,27 @@ function palindrama(str){
 	else{
 		return true;
 	}
+function palindrama(str){
+	bag="";
+	for(i=0;i<str.length;i++){
+		bag=bag+str[i];
+	}
+	bagr="";
+	for(j=str.length-1;j>=0;j--){
+		bagr=bagr+str[j];
+	}
+	if(bag==bagr){
+		return true;	
+	}
+	else{
+		return false;
+	}	
+}
+
+
+p_check="maddam";
+result_pal=palindrama(p_check);
+console.log(result_pal);
 
 
 	
