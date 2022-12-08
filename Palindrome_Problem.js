b2da1463eeb6e88692a884177c077bee149e8317
MangeshAwate <mawate9@gmail.com>
2022-12-08 14:32:45 +0530

    palindrome fix

--- Palindrome_Problem.js
+++ Palindrome_Problem.js
@@ -1,19 +1,17 @@
-function palindrama(str){
+y#yop
+function palindrome(string){
 	bag="";
-	for(i=0;i<str.length;i++){
+	&for(i=0;i<string.length;i++){
 		bag=bag+string[i];
 	}
 	bagr="";
-	for(j=string.length;j>0;j++){
-		bagr=bagr+string[i];
+	for(j=string.length-1;j>=0;j--){
+	&	bagr=bagr+string[i];
 	}
 	if(bag==bagr){
-		return false;	
+		return true;	
 	}
 	else{
-		return true;
+		return false;
 	}
-
-
-	
 }
